bf582ddd0659706c3fada740ecbf9f4c22ba4aca
Han Yeong-woo <han@yeongwoo.dev>
2022-04-08 12:57:38 +0900

    fix: move styles outside of pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import GlobalStyle from './styles/GlobalStyle';
+import GlobalStyle from '../styles/GlobalStyle';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
