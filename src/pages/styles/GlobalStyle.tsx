91f9ff6f2b2d4fc90c596791bfcbd578a9f4a1ea
Han Yeong-woo <han@yeongwoo.dev>
2022-04-08 12:00:48 +0900

    feat: add global css

---
+++ src/pages/styles/GlobalStyle.tsx
@@ -0,0 +1,69 @@
+import * as styled from 'styled-components';
+
+export default styled.createGlobalStyle`
+  html,
+  body,
+  p,
+  ol,
+  ul,
+  li,
+  dl,
+  dt,
+  dd,
+  blockquote,
+  figure,
+  fieldset,
+  legend,
+  textarea,
+  pre,
+  iframe,
+  hr,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+    padding: 0;
+  }
+
+  button,
+  input,
+  select {
+    margin: 0;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  #__next {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  body {
+    font-family: Pretendard, NanumGothic, sans-serif;
+    line-height: 1.5;
+    background-color: #fff;
+  }
+
+  code,
+  pre,
+  samp,
+  var {
+    font-family: 'JetBrains Mono NL', D2Coding, monospace;
+  }
+
+  a {
+    color: inherit;
+  }
+`;
