70d95e7cd72a33cd96dccebaffe6cb99b4f3fcd0
Nix <nix6839@washnix.com>
2022-04-12 09:08:33 +0900

    refactor: refactor generate-json-api code (#1)

---
+++ src/lib/PostConnector.ts
@@ -0,0 +1,151 @@
+import {
+  IsBoolean,
+  IsString,
+  validateSync,
+  ValidationError,
+} from 'class-validator';
+import { readdirSync, readFileSync } from 'fs';
+import matter from 'gray-matter';
+import { join as pathJoin } from 'path';
+
+interface File {
+  id: string;
+  filePath: string;
+  content: string;
+}
+
+interface MarkdownFile<FrontMatterType extends object = Record<string, any>>
+  extends File {
+  frontMatter: FrontMatterType;
+}
+
+interface PostFileFrontMatter {
+  title: string;
+  draft: boolean;
+}
+
+type PostFile = MarkdownFile<PostFileFrontMatter>;
+
+export type Post = Omit<PostFile, 'filePath' | 'frontMatter'> &
+  Omit<PostFileFrontMatter, 'draft'>;
+
+function readFilesFromDir(dirPath: string): File[] {
+  const fileInfos = readdirSync(dirPath).map((fileName) => ({
+    id: fileName.replace(/\.md$/, ''),
+    filePath: pathJoin(dirPath, fileName),
+  }));
+  const files = fileInfos.map<File>((fileInfo) => ({
+    ...fileInfo,
+    content: readFileSync(fileInfo.filePath, 'utf-8'),
+  }));
+  return files;
+}
+
+function fileToMarkdownFile({
+  id,
+  filePath,
+  content: fileContent,
+}: File): MarkdownFile {
+  const { content, data: frontMatter } = matter(fileContent);
+  return { id, filePath, content, frontMatter };
+}
+
+class PostFrontMatterValidator implements PostFileFrontMatter {
+  constructor(public readonly filePath: string, obj: Record<string, any>) {
+    this.title = obj.title;
+    this.draft = obj.draft;
+  }
+
+  @IsString()
+  title: string;
+
+  @IsBoolean()
+  draft: boolean;
+
+  public validate(): ValidationError[] {
+    return validateSync(this, { forbidUnknownValues: true });
+  }
+}
+
+function markdownFileIsValidPostFile(
+  markdownFile: MarkdownFile,
+): asserts markdownFile is PostFile {
+  const validationErrors = new PostFrontMatterValidator(
+    markdownFile.filePath,
+    markdownFile.frontMatter,
+  ).validate();
+  if (validationErrors.length > 0) {
+    throw validationErrors;
+  }
+}
+
+function mapDefaultPostFileValueToMarkdownFile<T extends MarkdownFile>(
+  markdownFile: T,
+): T & { frontMatter: { draft: boolean } } {
+  return {
+    ...markdownFile,
+    frontMatter: {
+      draft: false,
+      ...markdownFile.frontMatter,
+    },
+  };
+}
+
+function filesToPostFiles(files: File[]): PostFile[] {
+  const markdownFiles = files
+    .map(fileToMarkdownFile)
+    .map(mapDefaultPostFileValueToMarkdownFile);
+
+  const postFiles: PostFile[] = [];
+  const validationErrorsList: ValidationError[][] = [];
+  markdownFiles.forEach((markdownFile) => {
+    try {
+      markdownFileIsValidPostFile(markdownFile);
+      postFiles.push(markdownFile);
+    } catch (validationErrors: any) {
+      validationErrorsList.push(validationErrors);
+    }
+  });
+  if (validationErrorsList.length > 0) {
+    throw validationErrorsList;
+  }
+  return postFiles;
+}
+
+function getPostFilesFromDir(dirPath: string): PostFile[] {
+  const files = readFilesFromDir(dirPath);
+  const postFiles = filesToPostFiles(files);
+  return postFiles;
+}
+
+function getPubPostsFromPostFiles(postFiles: PostFile[]): Post[] {
+  const pubPostFiles = postFiles.filter(
+    (markdownFile) => markdownFile.frontMatter.draft === false,
+  );
+  const posts = pubPostFiles.map<Post>((postFile) => {
+    const { filePath, frontMatter, ...postData } = postFile;
+    const { draft, ...postInfo } = frontMatter;
+    return { ...postData, ...postInfo };
+  });
+  return posts;
+}
+
+function getPubPostsFromDir(dirPath: string): Post[] {
+  const postFiles = getPostFilesFromDir(dirPath);
+  const pubPosts = getPubPostsFromPostFiles(postFiles);
+  return pubPosts;
+}
+
+export function getPubPosts() {
+  return getPubPostsFromDir(pathJoin(process.cwd(), 'content'));
+}
+
+const posts = getPubPosts();
+
+export function getIds(): string[] {
+  return posts.map((post) => post.id);
+}
+
+export function getOne(postId: string): Post | undefined {
+  return posts.find((post) => post.id === postId);
+}
