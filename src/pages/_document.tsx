ddbd8a0f8cc8d47d52a8461d1787f66e4a580cac
Nix <nix6839@washnix.com>
2022-04-16 14:42:42 +0900

    fix: fix FOUC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,55 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import React from 'react';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="ko">
-      <Head>
-        <meta name="theme-color" content="#78c84e" />
-        <meta property="og:site_name" content="한영우의 개발 블로그" />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://unpkg.com/pretendard@1.2.2/dist/web/static/pretendard-dynamic-subset.css"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          <meta name="theme-color" content="#78c84e" />
+          <meta property="og:site_name" content="한영우의 개발 블로그" />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://unpkg.com/pretendard@1.2.2/dist/web/static/pretendard-dynamic-subset.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
