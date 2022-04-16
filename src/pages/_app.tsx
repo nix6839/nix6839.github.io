bf140362fd772f89aec586a55097b12c9ade6b21
Han Yeong-woo <han@yeongwoo.dev>
2022-04-16 16:30:07 +0900

    feat: update global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,14 @@ import type { AppProps } from 'next/app';
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Layout from '../components/Layout';
-import GlobalStyle from '../styles/GlobalStyle';
+import '../styles/global.css';
+import '../styles/pretendard.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <>
-      <GlobalStyle />
       <QueryClientProvider client={queryClient}>
         <Layout>
           <Component {...pageProps} />
