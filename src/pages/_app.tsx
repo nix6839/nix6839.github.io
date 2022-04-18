696d13216b70d0cd365e86a77a86754e1d85f0bd
Han Yeong-woo <han@yeongwoo.dev>
2022-04-19 04:31:48 +0900

    feat: add `window.scrollY` history

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,55 @@
 import type { AppProps } from 'next/app';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
+import {
+  cutOverflow,
+  increaseCurrentIndex,
+  setCurrentIndex,
+  setCurrentScrollY,
+} from '../app/scrollYHistorySlice';
+import store from '../app/store';
 import '../styles/global.css';
 import '../styles/pretendard.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+  const router = useRouter();
+  const dispatch = store.dispatch;
+
+  useEffect(() => {
+    let popStated = false;
+
+    router.beforePopState(() => {
+      const nextHistoryIndex = window.history.state.idx;
+      dispatch(setCurrentScrollY(window.scrollY));
+      dispatch(setCurrentIndex(nextHistoryIndex));
+      popStated = true;
+      return true;
+    });
+
+    function handleRouteChangeStart() {
+      if (popStated) {
+        return;
+      }
+      dispatch(setCurrentScrollY(window.scrollY));
+      dispatch(cutOverflow());
+      dispatch(increaseCurrentIndex());
+    }
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+    };
+  }, [router, dispatch]);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </Provider>
   );
 }
