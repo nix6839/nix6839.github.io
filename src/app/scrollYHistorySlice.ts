acd63f73b47955ea448ef87ab8aaa6165a4691c2
Nix <nix6839@washnix.com>
2022-04-19 04:31:48 +0900

    feat: add `window.scrollY` history

---
+++ src/app/scrollYHistorySlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from './store';
+
+const initialState: {
+  currentIndex: number;
+  scrollYHistory: number[];
+} = {
+  currentIndex: 0,
+  scrollYHistory: [],
+};
+
+const scrollYHistorySlice = createSlice({
+  name: 'scrollPosition',
+  initialState,
+  reducers: {
+    setCurrentScrollY: (state, { payload: scrollY }: PayloadAction<number>) => {
+      state.scrollYHistory[state.currentIndex] = scrollY;
+    },
+
+    setCurrentIndex: (state, { payload: newIndex }: PayloadAction<number>) => {
+      state.currentIndex = newIndex;
+    },
+
+    increaseCurrentIndex: (state) => {
+      state.currentIndex += 1;
+    },
+
+    cutOverflow: (state) => {
+      state.scrollYHistory.splice(state.currentIndex + 1);
+    },
+  },
+});
+
+export const {
+  setCurrentScrollY,
+  setCurrentIndex,
+  increaseCurrentIndex,
+  cutOverflow,
+} = scrollYHistorySlice.actions;
+
+export const selectCurrentScrollY = ({ scrollYHistory: state }: RootState) =>
+  state.scrollYHistory[state.currentIndex] ?? 0;
+
+export default scrollYHistorySlice.reducer;
