ba6176b4bcf4806d422be32cdad634f2e6d9f49a
Han Yeong-woo <han@yeongwoo.dev>
2023-08-26 01:44:20 +0900

    chore: add prettier-plugin-tailwindcss

--- prettier.config.js
+++ prettier.config.js
@@ -1,15 +1,18 @@
 import hywConfig from '@nix6839/prettier-config';
 import * as pluginAstro from 'prettier-plugin-astro';
+import * as pluginTailwind from 'prettier-plugin-tailwindcss';
 
 /** @type {import('prettier').Config} */
 const config = {
   ...hywConfig,
 
+  plugins: [pluginTailwind],
+
   overrides: [
     {
       files: '*.astro',
       options: {
-        plugins: [pluginAstro],
+        plugins: [pluginAstro, pluginTailwind],
         parser: 'astro',
         astroAllowShorthand: true,
       },
