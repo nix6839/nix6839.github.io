f5c53868211f0255043054b17f0cc66ddd9b1a0e
Han Yeong-woo <han@yeongwoo.dev>
2023-08-25 22:38:39 +0900

    feat: add tailwindcss

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,13 @@
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yeongwoo.dev',
   compressHTML: true,
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
